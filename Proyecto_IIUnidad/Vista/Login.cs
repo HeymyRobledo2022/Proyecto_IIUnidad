9114b30548d4351aadd3aa1a0922781e6b2fc063
HeymyRobledo2022 <116927220+HeymyRobledo2022@users.noreply.github.com>
2022-10-28 23:04:33 -0600

    Primer codigo
    Estructura inicial del proyecto

---
+++ Proyecto_IIUnidad/Vista/Login.cs
@@ -0,0 +1,22 @@
+namespace Vista
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(669, 408);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
